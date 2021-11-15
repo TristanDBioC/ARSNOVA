3f3c568ee8c3b643dde843a87b48268c1c3c8a01
Chem <david.tristankyle@gmail.com>
2021-11-16 04:51:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/arsnova/LaunchActivity.kt
@@ -0,0 +1,21 @@
+package com.example.arsnova
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+
+    }
+
+    fun buttonSigninclick(view: View) {
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
+
+    }
+
+}
