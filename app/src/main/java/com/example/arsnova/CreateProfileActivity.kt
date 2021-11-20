f6f03d9b62aac7ff9a9feef6f3389709dea2318d
Chem <david.tristankyle@gmail.com>
2021-11-21 01:02:55 +0800

    Nov 21 | 1:02 | Create Profile link to database. Except Photo

--- app/src/main/java/com/example/arsnova/CreateProfileActivity.kt
+++ app/src/main/java/com/example/arsnova/CreateProfileActivity.kt
@@ -1,17 +1,11 @@
 package com.example.arsnova
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.View
 import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
-import com.google.common.collect.Iterables
-import com.google.common.collect.Iterables.toArray
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -21,6 +15,7 @@ import com.google.firebase.ktx.Firebase
 class CreateProfileActivity : AppCompatActivity() {
     private lateinit var user : FirebaseUser
     private lateinit var auth : FirebaseAuth
+    private val db = Firebase.firestore
     private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         if (uri != null) {
             findViewById<ImageView>(R.id.imageProfilePic).setImageURI(uri)
@@ -34,7 +29,7 @@ class CreateProfileActivity : AppCompatActivity() {
         user = auth.currentUser!!
 
         populateSpinners()
-        findViewById<EditText>(R.id.editTextemail).setText(user.email)
+        findViewById<EditText>(R.id.editTextProfileEmail).setText(user.email)
 
         val profilePic = findViewById<ImageView>(R.id.imageProfilePic)
         profilePic.setOnClickListener() {
@@ -45,7 +40,6 @@ class CreateProfileActivity : AppCompatActivity() {
     }
 
     private fun populateSpinners() {
-        val db = Firebase.firestore
         var courses = arrayListOf<String>()
         var yearLevels = arrayListOf<String>()
 
@@ -81,6 +75,52 @@ class CreateProfileActivity : AppCompatActivity() {
                 findViewById<Spinner>(R.id.spinnerYearLevel).adapter = yearLevelsAdapter
             }
 
+    }
+
+    fun buttonFinished(view: View) {
+        if (!checkField()) {
+            addFirestoreUser()
+        }
+    }
+
+    private fun addFirestoreUser() {
+        val newUser = hashMapOf(
+            getString(R.string.user_firstname) to findViewById<EditText>(R.id.editTextFirstName).text.toString(),
+            getString(R.string.user_lastname) to findViewById<EditText>(R.id.editTextLastName).text.toString(),
+            getString(R.string.user_mi) to findViewById<EditText>(R.id.editTextMI).text.toString(),
+            getString(R.string.user_email) to findViewById<EditText>(R.id.editTextProfileEmail).text.toString(),
+            getString(R.string.user_dept) to findViewById<Spinner>(R.id.spinnerCourse).selectedItem.toString(),
+            getString(R.string.user_yearLevel) to findViewById<Spinner>(R.id.spinnerYearLevel).selectedItem.toString(),
+            getString(R.string.user_bio) to findViewById<EditText>(R.id.editTextBio).text.toString(),
+            getString(R.string.user_fines) to 0.0,
+            getString(R.string.user_incentives) to 0.0,
+            getString(R.string.user_type) to "regular",
+            getString(R.string.user_position) to ""
+        )
+        db.collection(getString(R.string.collection_users)).document(user.uid).set(newUser)
+            .addOnSuccessListener { Toast.makeText(this, "Profile successfully created", Toast.LENGTH_LONG).show() }
+            .addOnFailureListener {Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()}
+
+    }
+
+    private fun checkField() : Boolean{
+        var thereIsError = false;
+        val firstName = findViewById<EditText>(R.id.editTextFirstName)
+        val lastName = findViewById<EditText>(R.id.editTextLastName)
+        val MI = findViewById<EditText>(R.id.editTextMI)
+        val bio = findViewById<EditText>(R.id.editTextBio)
+
+        if (firstName.text.length == 0) {
+            firstName.error = "Must not be empty"
+            thereIsError = true
+        }
+
+        if (lastName.text.length == 0) {
+            lastName.error = "Must nt be empty"
+            thereIsError = true
+        }
+
+        return thereIsError
     }
 
 }
