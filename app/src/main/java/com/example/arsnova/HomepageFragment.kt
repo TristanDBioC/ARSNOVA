c8f42e5024176b01c4e734fe6c8c05064a2e0f1b
Chem <david.tristankyle@gmail.com>
2021-12-08 22:10:25 +0800

    Dec 8 10:09 | Homepage show available event list

--- app/src/main/java/com/example/arsnova/HomepageFragment.kt
+++ app/src/main/java/com/example/arsnova/HomepageFragment.kt
@@ -1,35 +1,40 @@
 package com.example.arsnova
 
 import android.content.pm.PackageManager
+import android.graphics.Typeface
 import android.os.Bundle
+import android.print.PrintAttributes
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat.getColor
+import androidx.core.view.marginBottom
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.example.arsnova.viewmodels.UserInfo
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import de.hdodenhof.circleimageview.CircleImageView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomepageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomepageFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private val viewModel : UserInfo by activityViewModels()
+    private val auth = Firebase.auth
+    private val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,7 +64,88 @@ class HomepageFragment : Fragment() {
             .setOnClickListener() {
                 (activity as HomepageActivity)!!.replaceFragment(ScannerFragment(), "Scan Attendance QR")
             }
+
+        populateScrollview(view.findViewById(R.id.homepageScrollView))
         return view
     }
 
+    private fun createEventCard(name: String, date: String, start: String, end: String, marked: Boolean): CardView {
+        // Main Card View
+        val MainCard = CardView(requireActivity())
+        MainCard.id = View.generateViewId()
+        MainCard.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.white))
+
+        val layoutparams  = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        layoutparams.setMargins(0, 0, 0, 10)
+        MainCard.layoutParams = layoutparams
+
+        // Linear Layout to contain elements
+        val linearlayout = LinearLayout(activity)
+        linearlayout.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+        )
+
+        linearlayout.orientation = LinearLayout.VERTICAL
+        linearlayout.setPadding(30, 10, 10, 10)
+
+        // TextView for EventName
+        val eventNameTextView = TextView(activity)
+        eventNameTextView.text = name
+        eventNameTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18.toFloat())
+        eventNameTextView.setTypeface(Typeface.DEFAULT_BOLD)
+
+        // TextView for DateTime
+        val DateTimeTextView = TextView(activity)
+        DateTimeTextView.text = ("%s | %s to %s".format(date, start, end))
+
+
+        // TextView for Attendance Status
+        val AttendanceTextView = TextView(activity)
+        if (marked) {
+            AttendanceTextView.text = ("Attendance is marked as present")
+        } else {
+            AttendanceTextView.text = ("Attendance is not marked as present")
+        }
+
+        linearlayout.addView(eventNameTextView)
+        linearlayout.addView(DateTimeTextView)
+        linearlayout.addView(AttendanceTextView)
+        MainCard.addView(linearlayout)
+        return MainCard
+    }
+
+    private fun populateScrollview(view: LinearLayout) {
+        db.collection(getString(R.string.collection_events))
+            .whereEqualTo(getString(R.string.event_availability), true)
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val name = document.data.getValue(getString(R.string.event_name)).toString()
+                    val date = document.data.getValue(getString(R.string.event_date)).toString()
+                    val start = document.data.getValue(getString(R.string.event_timeStart)).toString()
+                    val end = document.data.getValue(getString(R.string.event_timeEnd)).toString()
+                    val eventId = document.data.getValue(getString(R.string.event_qrcode)).toString()
+                    val marked = checkIfPresent(auth.currentUser!!.uid, eventId)
+                    println(name)
+                    view.addView(createEventCard(name, date, start, end, marked))
+                }
+            }
+    }
+
+    private fun checkIfPresent(uid: String, eventID: String): Boolean {
+        var returnValue = false
+        db.collection(getString(R.string.collection_attendance))
+            .whereEqualTo(getString(R.string.attendance_userID), uid)
+            .whereEqualTo(getString(R.string.attendance_eventId), eventID)
+            .get()
+            .addOnSuccessListener {
+                returnValue = !it.isEmpty
+            }
+
+        return returnValue
+    }
 }
