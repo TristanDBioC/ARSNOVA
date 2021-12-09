ecebb0ecb124bf0e9be608f30461f125d659bfad
Chem <david.tristankyle@gmail.com>
2021-12-09 23:51:32 +0800

    Dec 9 23:50 | Date format update

--- app/src/main/java/com/example/arsnova/ViewAttendanceFragment.kt
+++ app/src/main/java/com/example/arsnova/ViewAttendanceFragment.kt
@@ -194,7 +194,7 @@ class ViewAttendanceFragment : Fragment() {
     fun getDate(timestamp: Long) :String {
         val calendar = Calendar.getInstance(Locale.ENGLISH)
         calendar.timeInMillis = timestamp * 1000L
-        val date = DateFormat.format("dd-MM-yyyy | HH:mm:ss",calendar).toString()
+        val date = DateFormat.format("MM-dd-yyyy | HH:mm:ss",calendar).toString()
         return date
     }
 
